c077230b4d4503204742dde567b1123ae5509078
Abraham Vijai <abrahamvijai03@gmail.com>
2024-10-31 00:01:55 -0400

    Modularized Code

--- sketch.js
+++ sketch.js
@@ -1,85 +1,80 @@
-const CLOCK_RAIDUS = 300;
+const CLOCK_RADIUS = 300;
 const CLOCK_ANGLE = 30;
 const TEXT_SIZE = 15;
 const TIME_ARRAY = ['1','2','3','4','5','6','7','8','9','10','11','12'];
-  
+const NUM_TICKS_HOUR = 12;
+const NUM_TICKS_MINUTE = 60;
+
 function setup() {
   createCanvas(500, 500);
-  background(220);
+  background(150);
 }
 
-function draw(){
-  let secondAngle = map(second(), 0, 60, 0, 360);
-  let minuteAngle = map(minute(), 0, 60, 0, 360);
-  let hourAngle = map(hour(), 0, 12, 0, 360);
-
-  // Draw the clock
-  drawClock()
-
-  // Draw the numbers
+function draw() {
+  background(150);
+  drawClock();
   drawNumbers();
-
-  // Draw the Ticks
-  drawTicks(12, CLOCK_ANGLE, 140) 
-  drawTicks(60, 6,147) 
-
-  // Draw the second hand
-  drawHands(secondAngle, 1.5, 'red');
-  drawHands(minuteAngle,3, 'blue');
-  drawHands(hourAngle, 6, 'green');
-
+  drawTicks(NUM_TICKS_HOUR, CLOCK_ANGLE, 140);
+  drawTicks(NUM_TICKS_MINUTE, 6, 147);
+  
+  // Draw clock hands
+  drawClockHands();
 }
 
-function drawHands(handAngle, weight, color){
+function drawClockHands() {
+  const secondAngle = map(second(), 0, 60, 0, 360);
+  const minuteAngle = map(minute(), 0, 60, 0, 360);
+  const hourAngle = map(hour() % 12, 0, 12, 0, 360);
+
+  drawHand(secondAngle, CLOCK_RADIUS * 0.45, 1.5, 'red');
+  drawHand(minuteAngle, CLOCK_RADIUS * 0.4, 3, 'blue');
+  drawHand(hourAngle, CLOCK_RADIUS * 0.3, 6, 'green');
+}
+
+function drawHand(angle, length, weight, color) {
   push();
-  translate(width/2,height/2);
-  rotate(handAngle);
-  strokeWeight(weight)
-  stroke(color)
-  line(0,0, 0, -(CLOCK_RAIDUS/2)+TEXT_SIZE*2);
+  translate(width / 2, height / 2);
+  rotate(radians(angle - 90));
+  strokeWeight(weight);
+  stroke(color);
+  line(0, 0, length, 0);
   pop();
 }
 
 function drawClock() {
-  // Draw the clock
-  ellipse(width/2,height/2, CLOCK_RAIDUS, CLOCK_RAIDUS);
+  fill('aquamarine');
+  ellipse(width / 2, height / 2, CLOCK_RADIUS, CLOCK_RADIUS);
+  stroke('black');
 }
 
 function drawNumbers() {
-  // Add numbers
   for (let i = 1; i <= 12; i++) {
-    push();
-    // Translate the origin to the center
-    translate(width/2,height/2);
-    
-    // Rotate 30 degrees by the origin 
-    angleMode(DEGREES);
-    rotate(CLOCK_ANGLE*i);
-
-    // Draw the numbers
-    strokeWeight(3);
-    textAlign(CENTER);
-    textSize(TEXT_SIZE);
-    text(TIME_ARRAY[i-1],0, -CLOCK_RAIDUS/2+25);
-
-    pop();
+    drawTextAtAngle(i * CLOCK_ANGLE, TIME_ARRAY[i - 1], TEXT_SIZE, 'yellow');
   }
 }
 
-function drawTicks(numTicks, tickAngle, tickLength){
+function drawTicks(numTicks, tickAngle, tickLength) {
   for (let i = 0; i < numTicks; i++) {
-    push();
-    // Translate the origin to the center
-    translate(width/2,height/2);
-    
-    // Rotate 30 degrees by the origin 
-    angleMode(DEGREES);
-    rotate(tickAngle*i);
-
-    // Draw the ticks
-    strokeWeight(2);
-    line(0,-CLOCK_RAIDUS/2, 0, -tickLength)
-
-    pop();
+    drawLineAtAngle(i * tickAngle, tickLength);
   }
+}
+
+function drawTextAtAngle(angle, textContent, size, fillColor) {
+  push();
+  translate(width / 2, height / 2);
+  rotate(radians(angle - 90));
+  fill(fillColor);
+  textSize(size);
+  textAlign(CENTER);
+  text(textContent, 0, -CLOCK_RADIUS / 2 + 25);
+  pop();
+}
+
+function drawLineAtAngle(angle, length) {
+  push();
+  translate(width / 2, height / 2);
+  rotate(radians(angle - 90));
+  strokeWeight(2);
+  line(0, -CLOCK_RADIUS / 2, 0, -length);
+  pop();
 }
