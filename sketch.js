cb93cfb16fd9b2e17a18fab7ca0e113114151600
Abraham Vijai <abrahamvijai03@gmail.com>
2024-10-30 21:36:03 -0400

    Added circle

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+
+  const CLOCK_RAIDUS = 300;
+
+  // Circle
+  ellipse(width/2,height/2, CLOCK_RAIDUS, CLOCK_RAIDUS);
+
+}
+
+function draw() {
+}
